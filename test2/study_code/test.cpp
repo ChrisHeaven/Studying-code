a306dc7729741ca877291c8df4087fc8250e46b7
Yizhang Peng <yp16505@my.bristol.ac.uk>
2017-07-27 21:53:36 +0100

    最大差值

--- test2/study_code/test.cpp
+++ test2/study_code/test.cpp
@@ -5,56 +5,29 @@ using namespace std;
 
 int a[1000][1000];
 
-int last_num(int num)
-{
-    if (num > 1000)
-        num = 1000;
-
-    int del_point = 2;
-    int arr_index = 0;
-    int count = 0;
-
-    for (int i = 0; i < num; i++) {
-        a[arr_index][i] = i;
-    }
-
-    int sub_count = 0;
-    while (1) {
-        if (num - count == 1)
-            break;
-
-        if (del_point <= (num - 1 - count)) {
-            a[arr_index][del_point] = -1;
-            sub_count++;
-            del_point = del_point + 3;
-        }
-        else {
-            del_point = del_point - (num - count);
-
-            int j = 0;
-            for (int i = 0; i < num; i++) {
-                if (a[arr_index][i] != -1) {
-                    a[arr_index + 1][j] = a[arr_index][i];
-                    j++;
-                }
+class LongestDistance {
+public:
+    int getDis(vector<int> A, int n) {
+        // write code here
+        int max_dis = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i; j < n; j++)
+            {
+                if (A[j] - A[i] >= max_dis)
+                    max_dis = A[j] - A[i];
             }
-            arr_index++;
-            count = count + sub_count;
-            sub_count = 0;
         }
-        if (num - count == 1)
-            break;
+        return max_dis;
     }
-
-    int value_ = a[arr_index][0];
-    return value_;
-}
+};
 
 int main()
 {
     int n;
     while (cin >> n) {
-        cout << last_num(n) << endl;
+
+        cout << getDis(n) << endl;
     }
 
     return 0;
