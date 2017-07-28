a1485ac8325484c43c880bd8970571380be44308
Yizhang Peng <yp16505@my.bristol.ac.uk>
2017-07-28 23:25:48 +0100

    回文串

--- test2/Source.cpp
+++ test2/Source.cpp
@@ -1,65 +1,28 @@
-#include <iostream>
-#include <stdlib.h>
-#include <vector>
-#include <string>
 
+#include<iostream>
+#include<string>
 using namespace std;
+int main() {
+	string str;
+	while (cin >> str) {
+		int i, l = str.length(), x = 2, j = l - 1;
+		for (i = 0; i < j; i++, j--) {
+			if (str[i] != str[j]) {
+				x--;
+				if (str[i + 1] == str[j])
+					i++;
+				else if (str[i] == str[j - 1])
+					j--;
+				else
+					x--;
 
-class Solution {
-public:
-	/**
-	* 计算你能获得的最大收益
-	*
-	* @param prices Prices[i]即第i天的股价
-	* @return 整型
-	*/
-	int calculateMax(vector<int> prices) {
-		int first_max = 0;
-		int second_max = 0;
-		int sum = 0, sum_1 = 0, sum_2 = 0;
-
-		for (int i = 0; i < prices.size(); i++)
-		{
-			for (int j = 0; j < i; j++) {
-				for (int k = j + 1; k < i; k++)
-					if (prices[k] - prices[j] > first_max)
-						first_max = prices[k] - prices[j];
-			}
-			for (int j = i; j < prices.size(); j++) {
-				for (int k = j + 1; k < prices.size(); k++)
-					if (prices[k] - prices[j] > second_max)
-						second_max = prices[k] - prices[j];
 			}
 
-			if (first_max > 0)
-				sum_1 = first_max;
-			if (second_max > 0)
-				sum_2 = second_max;
-
-			if (sum_1 + sum_2 >= sum)
-				sum = sum_1 + sum_2;
-
-			first_max = 0;
-			second_max = 0;
-			sum_1 = 0;
-			sum_2 = 0;
 		}
-		return sum;
+		if (x > 0)
+			cout << "YES" << endl;
+		else
+			cout << "NO" << endl;
+
 	}
-};
-
-int main()
-{
-	//vector<int> prices = { 61,8,81,67,80,47 };
-	//Solution s;
-	//cout << s.calculateMax(prices) << endl;
-	//system("PAUSE");
-
-	string a;
-	while (cin >> a)
-	{
-		cout << a[1] << endl;
-	}
-
-	return 0;
 }
--- test2/study_code/test.cpp
+++ test2/study_code/test.cpp
@@ -1,58 +1,58 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
+#include <string>
 
 using namespace std;
 
-class Solution {
-public:
-    /**
-    * 计算你能获得的最大收益
-    *
-    * @param prices Prices[i]即第i天的股价
-    * @return 整型
-    */
-    int calculateMax(vector<int> prices) {
-        int first_max = 0;
-        int second_max = 0;
-        int sum = 0, sum_1 = 0, sum_2 = 0;
+int get_answer(string str) {
+    int yes_ = 0;
 
-        for (int i = 0; i < prices.size(); i++)
-        {
-            for (int j = 0; j < i; j++) {
-                for (int k = j + 1; k < i; k++)
-                    if (prices[k] - prices[j] > first_max)
-                        first_max = prices[k] - prices[j];
+    for (int i = 0; i < str.size(); i++)
+    {
+        int k = 0;
+        char backup[10];
+
+        for (int j = 0; j < str.size(); j++) {
+            if (j != i) {
+                backup[k] = str[j];
+                k++;
             }
-            for (int j = i; j < prices.size(); j++) {
-                for (int k = j + 1; k < prices.size(); k++)
-                    if (prices[k] - prices[j] > second_max)
-                        second_max = prices[k] - prices[j];
-            }
-
-            if (first_max > 0)
-                sum_1 = first_max;
-            if (second_max > 0)
-                sum_2 = second_max;
-
-            if (sum_1 + sum_2 >= sum)
-                sum = sum_1 + sum_2;
-
-            first_max = 0;
-            second_max = 0;
-            sum_1 = 0;
-            sum_2 = 0;
         }
-        return sum;
+
+        int num;
+        if ((str.size() - 1) % 2 == 0)
+            num = (str.size() - 1) / 2;
+        else
+            num = (str.size() - 1) / 2 + 1;
+
+        int count = 0;
+
+        for (int j = 0; j < num; j++) {
+            if (backup[j] == backup[(str.size() - 1) - 1 - j])
+                count++;
+            else
+                break;
+        }
+        if (count == num) {
+            yes_ = 1;
+            break;
+        }
     }
-};
+    if (yes_)
+        return 1;
+    else
+        return 0;
+}
 
 int main()
 {
-    vector<int> prices = { 61, 8, 81, 67, 80, 47 };
-    Solution s;
-    cout << s.calculateMax(prices) << endl;
-    system("PAUSE");
+    string a;
+    while (cin >> a)
+        if (get_answer(a))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
 
     return 0;
 }
