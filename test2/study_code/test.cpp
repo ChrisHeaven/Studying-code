ae60bc9a8b67a66814ba28e1f1c8b03df35d43ff
Yizhang Peng <yp16505@my.bristol.ac.uk>
2017-07-29 19:15:20 +0100

    钓鱼比赛

--- test2/study_code/test.cpp
+++ test2/study_code/test.cpp
@@ -3,34 +3,57 @@
 #include <vector>
 #include <string>
 #include <math.h>
+#include<iomanip>
 
 using namespace std;
 
-int move(int r, double point_1[], double point_2[]) {
-    double dis = sqrt((point_1[0] - point_2[0]) * (point_1[0] - point_2[0]) + (point_1[1] - point_2[1]) * (point_1[1] - point_2[1]));
-    int pre_step = dis / (2 * r);
-    int step = 0;
-    if (dis > pre_step * (2 * r))
-        step = pre_step + 2;
-    else
-        step = pre_step;
-    return step;
-}
+float poss[1000][1000];
+float max_poss = 0;
 
 int main()
 {
-    int r;
-    double point_1[2], point_2[2];
-    while (cin >> r) {
-        for (int i = 0; i < 2; i++)
+    int str[5];
+    while (cin >> str[0]) {
+        for (int i = 1; i < 5; i++)
         {
-            cin >> point_1[i];
+            cin >> str[i];
         }
-        for (int i = 0; i < 2; i++)
-        {
-            cin >> point_2[i];
+
+        for (int i = 0; i < str[0]; i++) {
+            for (int j = 0; j < str[1]; j++) {
+                cin >> poss[i][j];
+            }
         }
-        cout << move(r, point_1, point_2) << endl;
+
+        float cc_poss = 0, ss_poss = 0;
+        cc_poss = poss[str[2] - 1][str[3] - 1];
+        float sum = 0;
+
+        for (int i = 0; i < str[0]; i++) {
+            for (int j = 0; j < str[1]; j++) {
+                sum = sum + poss[i][j];
+            }
+        }
+
+        ss_poss = sum / (str[0] * str[1]);
+
+        cc_poss = 1 - pow(1 - cc_poss, str[4]);
+        ss_poss = 1 - pow(1 - ss_poss, str[4]);
+
+        if (ss_poss > cc_poss) {
+            max_poss = ss_poss;
+            cout << "ss" << endl;
+        }
+        else if (cc_poss > ss_poss) {
+            max_poss = cc_poss;
+            cout << "cc" << endl;
+        }
+        else if (cc_poss == ss_poss) {
+            max_poss = cc_poss;
+            cout << "equal" << endl;
+        }
+
+        cout << fixed << setprecision(2) << max_poss << endl;
     }
 
     return 0;
