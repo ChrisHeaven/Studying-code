c264897c1542fd2131f5cf5c21ca99e2a187a409
Yizhang Peng <yp16505@my.bristol.ac.uk>
2017-08-03 22:15:19 +0100

    最后一个字符

--- test2/Source.cpp
+++ test2/Source.cpp
@@ -9,82 +9,48 @@
 
 using namespace std;
 
-class Coder {
-public:
-	vector<string> findCoder(vector<string> A, int n) {
-		// write code here
-		int num[300] = { 0 }, index[300] = { 0 };
-		vector<string> A_buff;
+char string_[20][1000000] = { "" };
 
-		for (int i = 0; i < n; i++)
+int get_secret(int t)
+{
+	char secret[20] = { "" };
+	for (int i = 0; i < t; i++)
+	{
+		for (int j = 0; j < 1000000; j++)
 		{
-			A_buff.push_back(A[i]);
-			for (int j = 0; j < A[i].size(); j++)
+			int flag = 0;
+			for (int k = 0; k < 1000000; k++)
 			{
-				if (A[i][j] >= 'a' && A[i][j] <= 'z')
-					A[i][j] += ('A' - 'a');
-			}
-
-			int count = 0, pos = 0;
-			for (int j = 0; j < A[i].size(); j++)
-			{
-				if (A[i].find("CODER", pos) == -1)
-					break;
-				pos = A[i].find("CODER", pos) + 1;
-				count++;
-			}
-			num[i] = count;
-		}
-
-		for (int i = 0; i < 300; i++)
-		{
-			int num_count = 0;
-			for (int j = 0; j < 300; j++)
-			{
-				if (num[i] >= num[j])
-					num_count++;
-			}
-			if (index[300 - num_count] == 0)
-				index[300 - num_count] = i + 1;
-			else
-			{
-				for (int k = 300 - num_count; k < 300; k++)
+				if (string_[i][j] == string_[i][k] && j != k)
 				{
-					if (index[k] == 0)
-					{
-						index[k] = i + 1;
-						break;
-					}
+					flag = 1;
+					break;
 				}
 			}
+			if (flag == 0)
+			{
+				secret[i] = string_[i][j];
+				break;
+			}
 		}
-
-		int zero_count = 0;
-		for (int i = 0; i < 300; i++)
-		{
-			if (num[i] > 0)
-				zero_count++;
-		}
-
-		vector<string> B;
-		for (int i = 0; i < zero_count; i++)
-			B.push_back(A_buff[index[i] - 1]);
-
-		return B;
+		printf("%c\n", secret[i]);
 	}
-};
+
+	return 0;
+}
 
 int main()
 {
-	vector<string> A = { "i am a coder", "Coder Coder", "Code" };
-	int n = 3;
-	Coder code;
-	vector<string> B = code.findCoder(A, n);
+	int t;
+	while (cin >> t)
+	{
+		for (int i = 0; i < t; i++)
+		{
+			scanf("%s", string_[i]);
+		}
 
-	for (int i = 0; i < B.size(); i++)
-		cout << B[i] << endl;
-
-	system("pause");
+		get_secret(t);
+	}
 
 	return 0;
 }
