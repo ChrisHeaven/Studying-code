558839ad5dbc42ef5dac405b3bfaf492ddc98d76
Yizhang Peng <yp16505@my.bristol.ac.uk>
2017-08-18 23:36:26 +0100

    拓扑排序

--- test2/Source.cpp
+++ test2/Source.cpp
@@ -9,140 +9,49 @@
 
 using namespace std;
 
-struct dll_node
-{
-	int value;
-	dll_node *next;
-	dll_node *pre;
-};
+int relation[6][6] = {0};
 
-dll_node *create_dll_node(int data[], int len)
+void add_edge(int a, int b)
 {
-	dll_node *p = new dll_node();
-	p->pre = NULL;
-	p->next = NULL;
-	p->value = data[0];
-	dll_node *head = p;
-
-	for (int i = 1; i < len; i++)
-	{
-		dll_node *new_node = new dll_node();
-		new_node->value = data[i];
-		new_node->pre = p;
-		new_node->next = NULL;
-		p->next = new_node;
-		p = new_node;
-	}
-	return head;
+	relation[a][b + 1]++;
+	relation[b][0]++;
 }
 
-void print_list(dll_node *head)
+void top_sort()
 {
-	for (dll_node *i = head; i != NULL; i = i->next)
-		cout << i->value << " ";
+	int n = sizeof(relation[0]) / sizeof(int);
+	int count = n;
+
+	while (count > 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			if (relation[i][0] == 0)
+			{
+				cout << i << " ";
+				relation[i][0] = -1;
+				for (int j = 1; j < n; j++)
+				{
+					if (relation[i][j] > 0)
+						relation[j - 1][0]--;
+				}
+				count--;
+			}
+		}
+	}
 	cout << endl;
-}
-
-dll_node *insert_node_before(dll_node *p, int value, dll_node *head)
-{
-	dll_node *p_pre = p->pre;
-	dll_node *new_node = new dll_node();
-
-	if (p_pre != NULL)
-	{
-		p->pre = new_node;
-		new_node->next = p;
-		new_node->pre = p_pre;
-		p_pre->next = new_node;
-		new_node->value = value;
-	}
-	else
-	{
-		p->pre = new_node;
-		new_node->next = p;
-		new_node->pre = NULL;
-		new_node->value = value;
-		head = new_node;
-	}
-	return head;
-}
-
-void insert_node_behind(dll_node *p, int value)
-{
-	dll_node *p_next = p->next;
-	dll_node *new_node = new dll_node();
-
-	if (p_next != NULL)
-	{
-		p->next = new_node;
-		new_node->pre = p;
-		new_node->next = p_next;
-		p_next->pre = new_node;
-		new_node->value = value;
-	}
-	else
-	{
-		p->next = new_node;
-		new_node->next = NULL;
-		new_node->pre = p;
-		new_node->value = value;
-	}
-}
-
-dll_node *delete_list(dll_node *head, dll_node *delete_node)
-{
-	dll_node *p = delete_node;
-	dll_node *p_next = p->next;
-
-	if (p->next == NULL)
-		p->pre->next = p->next;
-	else if (p == head)
-	{
-		head = head->next;
-		delete p;
-		p = NULL;
-	}
-	else
-	{
-		if (p_next->next != NULL)
-			p_next->next->pre = p;
-		p->next = p_next->next;
-		p->value = p_next->value;
-		delete p_next;
-		p_next = NULL;
-	}
-
-	return head;
 }
 
 void main()
 {
-	int data[] = { 1, 2, 3, 4, 5, 6, 6, 6, 7, 7 };
-	int length = sizeof(data) / sizeof(int);
-	dll_node *head = create_dll_node(data, length);
-	print_list(head);
-	head = insert_node_before(head, 8, head);
-	print_list(head);
+	add_edge(5, 2);
+	add_edge(5, 0);
+	add_edge(4, 0);
+	add_edge(4, 1);
+	add_edge(2, 3);
+	add_edge(3, 1);
 
-	for (dll_node *i = head; i != NULL; i = i->next)
-	{
-		if (i->value == 3)
-			insert_node_behind(i, 23);
-	}
-	print_list(head);
-
-	head = delete_list(head, head);
-	print_list(head);
-
-	for (dll_node *i = head; i != NULL; i = i->next)
-	{
-		if (i->value == 7)
-		{
-			head = delete_list(head, i);
-			i = i->pre;
-		}
-	}
-	print_list(head);
+	top_sort();
 
 	system("pause");
 }
