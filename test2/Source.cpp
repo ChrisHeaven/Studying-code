4dd3d848f5cdf296f4ac9327285075e4a9c44f66
Yizhang Peng <yp16505@my.bristol.ac.uk>
2017-08-18 11:23:59 +0100

    双向链表

--- test2/Source.cpp
+++ test2/Source.cpp
@@ -9,59 +9,115 @@
 
 using namespace std;
 
-int sunday(string a, string b, int hash_table[])
+struct ddl_node
 {
-	int index = 0;
+	int value;
+	ddl_node *next;
+	ddl_node *pre;
+};
 
-	for (int i = 0, j = 0; i < a.size(), j < b.size();)
+ddl_node *create_ddl_node(int data[], int len)
+{
+	ddl_node *head = new ddl_node();
+	head = NULL;
+	ddl_node *p = head;
+
+	for (int i = 0; i < len; i++)
 	{
-		if (a[i] == b[j])
-		{
-			i++;
-			j++;
-			if (j == b.size())
-			{
-				index = i - j;
-				break;
-			}
-		}
+		ddl_node *node = new ddl_node();
+		node->value = data[i];
+		node->next = NULL;
+		node->pre = NULL;
+		if (head == NULL)
+			head = node;
 		else
 		{
-			if (i + b.size() < a.size())
-				i = i + b.size();
-			else
-			{
-				index = -1;
-				break;
-			}
-
-			while (i < a.size())
-			{
-				if (hash_table[(int)a[i]] > 0)
-				{
-					j = 0;
-					i = i - hash_table[a[i]] + 1;
-					break;
-				}
-				i++;
-			}
+			p->next = node;
+			node->pre = p;
 		}
+		p = node;
 	}
-	return index;
+	return head;
 }
 
-int main()
+void print_list(ddl_node *head)
 {
-	string a = "LESSONS SOFTWARE% TEARNED IN SOFTWARE% TE";
-	string b = "SOFTWARE%";
-	int hash_table[128] = {0};
+	while (head != NULL)
+	{
+		cout << head->value << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
 
-	for (int i = 0; i < b.size(); i++)
-		hash_table[(int)b[i]] = i + 1;
+void insert_node(ddl_node *p, int value)
+{
+	ddl_node *p_next = p->next;
+	ddl_node *new_node = new ddl_node();
 
-	cout << sunday(a, b, hash_table) << endl;
-	cout << a.find(b) << endl;
+	if (p_next != NULL)
+	{
+		p->next = new_node;
+		new_node->pre = p;
+		new_node->next = p_next;
+		p_next->pre = new_node;
+
+		new_node->value = value;
+	}
+	else
+	{
+		p->next = new_node;
+		new_node->next = NULL;
+		new_node->pre = p;
+		new_node->value = value;
+	}
+}
+
+void delete_list(ddl_node *head, ddl_node *p)
+{
+	if (p->next == NULL)
+	{
+		delete p;
+		p = NULL;
+	}
+	else if (p == head)
+	{
+		head = head->next;
+		delete p;
+		p = NULL;
+	}
+	else
+	{
+		p->pre->next = p->next;
+		p->next->pre = p->pre;
+		delete p;
+		p = NULL;
+	}
+}
+
+void main()
+{
+	int data[] = { 1, 2, 3, 4, 5, 6, 7 };
+	int length = sizeof(data) / sizeof(int);
+	ddl_node *head = create_ddl_node(data, length);
+	print_list(head);
+	insert_node(head, 8);
+	print_list(head);
+
+	ddl_node *find = head;
+	for (int i = 0; i < length; i++)
+	{
+		find = find->next;
+		if (find->value == 3)
+			insert_node(find, 23);
+	}
+	print_list(head);
+
+	//delete_list(head, head);
+	//print_list(head);
+
+	delete_list(head, head->next);
+	print_list(head);
 
 	system("pause");
-	return 0;
 }
